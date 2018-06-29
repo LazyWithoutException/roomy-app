419d1db8c6ea3c8d4a16b411d181836dbeaabe33
zen17 <blabla1710>
2018-06-29 13:56:38 +0200

    final
    final

---
+++ vratioglase.php
@@ -0,0 +1,25 @@
+<?php
+require_once('classes/lib.php');
+require_once('includes/config.php');
+include_once 'classes/password.php';
+
+if(isset($_GET["tip"])){
+    if($_GET["tip"] == "stan"){
+
+        echo $json=json_encode(DB::vratiStanISlike($stan));
+    }
+    else{
+        echo $json=json_encode(DB::vratiCimeraISlike($stan));
+    }
+}
+
+if(isset($_GET["atribut"])){
+    if($_GET["atribut"] == "stan"){
+        $json=json_encode(DB::vratiSveStanove(),true);
+        echo $json;
+    }
+    else{
+        $json=json_encode(DB::vratiSveCimere(),true);
+        echo $json;
+    }
+}
